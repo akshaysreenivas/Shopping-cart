d18fef68ca40bac3d167820094d40e18d06a667c
akshay <akshaypvkeerakkal@gmail.com>
2022-02-25 14:27:58 +0530

    connected with data base

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const { engine } = require('express-handlebars');
-var fileUpload=require('express-fileupload')
-var adminRouter = require('./routes/admin');
-var usersRouter = require('./routes/users');
-const { extname } = require('path');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { engine } = require("express-handlebars");
+var fileUpload = require("express-fileupload");
+var adminRouter = require("./routes/admin");
+var usersRouter = require("./routes/users");
+const { extname } = require("path");
+var db = require("./configuration/connection");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 // app.engine('handlebars', engine({ extname: '.hbs',partialsDir:__dirname+'/views/partials/'}));
 // app.set('view engine', 'handlebars');
-app.engine('hbs',engine({extname:'hbs',partialsDir:__dirname+'/views/partials/'}))
-app.use(logger('dev'));
+app.engine(
+  "hbs",
+  engine({ extname: "hbs", partialsDir: __dirname + "/views/partials/" })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload())
-app.use('/', usersRouter);
-app.use('/admin', adminRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(fileUpload());
+db.connect((err) => {
+  if (err) {
+    console.log("failed to connect to db😢");
+  } else {
+    console.log("connected to db port 27017");
+  }
+});
+
+app.use("/", usersRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/admin.js
+++ routes/admin.js
@@ -1,42 +1,56 @@
-var express = require('express');
-const res = require('express/lib/response');
+var express = require("express");
+const res = require("express/lib/response");
 var router = express.Router();
-
+var productHelper = require("../helpers/product-helpers");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  let products=[
+router.get("/", function (req, res, next) {
+  let products = [
     {
-      name:"i Phone 13",
-      category:"Mobile-phone",
-      desciption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
-    image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-blue-select-2021?wid=940&hei=1112&fmt=png-alpha&.v=1629842712000"
-     },
-    {
-      name:"one plus 9 pro",
-      category:"Mobile-phone",
-      desciption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
-      image:"https://m.media-amazon.com/images/I/612ytK4luvL._SL1500_.jpg"
+      name: "i Phone 13",
+      category: "Mobile-phone",
+      desciption:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
+      image:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-blue-select-2021?wid=940&hei=1112&fmt=png-alpha&.v=1629842712000",
     },
     {
-      name:"samsung s22 ultra ",
-      category:"Mobile-phone",
-      desciption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
-      image:"https://images.samsung.com/uk/smartphones/galaxy-s22-ultra/buy/02_carousel/04_basic-colors/s22_ultra_productkv_burgundy_mo.jpg"
+      name: "one plus 9 pro",
+      category: "Mobile-phone",
+      desciption:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
+      image: "https://m.media-amazon.com/images/I/612ytK4luvL._SL1500_.jpg",
     },
     {
-      name:"poco x3 pro",
-      category:"Mobile-phone",
-      desciption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
-      image:"https://m.media-amazon.com/images/I/61sj+WsNkCL._AC_SX522_.jpg"
-    }
-  ]
-  res.render('admin/view-products',{admin:true,products});
+      name: "samsung s22 ultra ",
+      category: "Mobile-phone",
+      desciption:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
+      image:
+        "https://images.samsung.com/uk/smartphones/galaxy-s22-ultra/buy/02_carousel/04_basic-colors/s22_ultra_productkv_burgundy_mo.jpg",
+    },
+    {
+      name: "poco x3 pro",
+      category: "Mobile-phone",
+      desciption:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo molestiae, exercitationem vel voluptate deleniti laboriosam laudantium",
+      image: "https://m.media-amazon.com/images/I/61sj+WsNkCL._AC_SX522_.jpg",
+    },
+  ];
+  res.render("admin/view-products", { admin: true, products });
 });
-router.get('/add-product',function(req, res, next){
-  res.render('admin/add-product')
+router.get("/add-product", function (req, res, next) {
+  res.render("admin/add-product");
 });
-router.post('/add-product',(req,res)=>{
-  console.log(req.body)
-  console.log(req.files.image)
+router.post("/add-product", (req, res) => {
+  productHelper.addProduct(req.body, (id) => {
+    let image = req.files.image;
+    image.mv("./public/product-images/" + id + ".jpg", (err) => {
+      if (!err) {
+        res.render("admin/add-product");
+      } else {
+        console.log(err);
+      }
+    });
+  });
 });
 module.exports = router;
